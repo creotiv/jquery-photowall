64478f914184b0f070bf054080025a66f84528fb
Andrey Nikishaev <creotiv@gmail.com>
2011-12-25 12:07:14 +0200

    Social buttons on change url fix

--- jquery-photowall.js
+++ jquery-photowall.js
@@ -414,8 +414,8 @@ var ShowBox = {
                 '<div id="showbox" style="display:none;">'
                 +'    <div id="showbox-exit"></div>'
                 +'    <div class="showbox-image unselect" unselectable="on">'
-                +'        <div id="showbox-loader"></div>'
                 +'    </div>'
+                +'    <div id="showbox-loader"></div>'
                 +'    <div class="showbox-menubar unselect" unselectable="on" style="display:none">'
                 +         ShowBox.options.menuBarContent
                 +'        </div>'
